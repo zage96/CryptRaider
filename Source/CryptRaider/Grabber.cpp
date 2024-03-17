8b13875aeb2fea62aa28733d2c6932ff4fa4f249
Enrique Zaragoza <zarag.enrique@gmail.com>
2024-03-17 16:24:50 -0500

    Section 4: 84-87
    Added statue and created a grabber class. The grabber class was added to the camera POV of the actor, and the rotation of it is being obtained. Added a trace collision called "Gabber". Added the library "Engine/World.h"  and 	#include "DrawDebugHelpers.h" to the Grabber.cpp. Created a pointer that shows a distance of 4 meters to where the actor is pointing at

---
+++ Source/CryptRaider/Grabber.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Grabber.h"
+#include "Engine/World.h"
+#include "DrawDebugHelpers.h"
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	
+	//FRotator MyRotation = GetComponentRotation();
+	//FString MyRotationString = MyRotation.ToCompactString();
+	//UE_LOG(LogTemp, Display, TEXT("Location Grabber: %s"),*MyRotationString);
+	
+	
+
+	//UWorld* MyWorld = GetWorld();
+	//float Time = MyWorld->TimeSeconds;
+	//UE_LOG(LogTemp, Display, TEXT("Current Time is %f"), Time);
+
+	FVector Start = GetComponentLocation();
+	FVector End = Start + GetForwardVector()*MaxGrabDistance;
+	DrawDebugLine(GetWorld(),Start,End, FColor::Red);
+}
+
